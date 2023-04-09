c1159272cce879ba09d680106b497c874ebb8e73
Anupam Bhakta <anupambhakta1@gmail.com>
2023-04-09 12:10:02 -0400

    added print cwd

--- src/minishell.c
+++ src/minishell.c
@@ -4,11 +4,13 @@
 #include <limits.h>
 
 int main(int argc, char **argv) {
-    char *cwd[PATH_MAX + 1];
+    char cwd[PATH_MAX + 1];
 
     // Main loop of minishell
     while (1) {
-        
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            printf("[%s]$", cwd);
+        }
     }
 
     return EXIT_SUCCESS;
