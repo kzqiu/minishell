448382baebd9266c1514c9b100dc61b10adb8cf3
Kevin Qiu <kzqiu01@gmail.com>
2023-04-19 17:40:49 -0400

    Solved relative paths (home) in cd and exec'ed functions.

--- src/minishell.c
+++ src/minishell.c
@@ -17,24 +17,37 @@
     cmd: full command passed to shell string
     cwd: current working directory string
 */
-void cd(char *cmd, char *cwd) {
+void cd(char *cmd) {
     char *new_path = cmd + 3;
 
+    struct passwd *usr;
+    if ((usr = getpwuid(getuid())) == NULL) {
+        fprintf(stderr, "\nError: Cannot get passwd entry. %s.\n", strerror(errno));
+        return;
+    }
+
     if (new_path[0] == '\0' || new_path[0] == '\n') { // Case: go to home dir
-        struct passwd *user;
-        if ((user = getpwuid(getuid())) == NULL) {
-            fprintf(stderr, "\nError: Cannot get passwd entry. %s.\n", strerror(errno));
-        } else {
-            chdir(user->pw_dir);
-        }
+        chdir(usr->pw_dir);
     } else { // Case: go to new_path (abs and relative)
         new_path[strlen(new_path) - 1] = '\0';
+
         for (int i = 0; i < strlen(new_path); i++) {
             if (new_path[i] == ' ') {
                 fprintf(stderr, "\nError: Too many arguments to cd.\n");
+                return;
             }
         }
-        if (chdir(new_path) != 0) {
+
+        char cd_path[PATH_MAX + 1];
+
+        if (new_path[0] == '~') {
+            strcpy(cd_path, usr->pw_dir);
+            strcpy(cd_path + strlen(cd_path), new_path + 1);
+        } else {
+            strcpy(cd_path, new_path);
+        }
+        
+        if (chdir(cd_path) != 0) {
             fprintf(stderr, "\nError: Cannot change directory to '%s'. %s.\n", new_path, strerror(errno));
         }
     }
@@ -48,7 +61,7 @@ void cd(char *cmd, char *cwd) {
 */
 void execute(char *cmd, char *cwd) {
     if (cmd[0] == 'c' && cmd[1] == 'd' && (cmd[2] == ' ' || cmd[2] == '\n')) { // Case 1: cd
-        cd(cmd, cwd);
+        cd(cmd);
     } else if (cmd[0] == 'e' && cmd[1] == 'x' && cmd[2] == 'i' && cmd[3] == 't' && (cmd[4] == ' ' || cmd[4] == '\n')) { // Case 2: exit
         printf("\n");
         exit(0);
@@ -59,11 +72,27 @@ void execute(char *cmd, char *cwd) {
         char *tmp = strtok(cmd, " ");
         int argc = 0;
 
+        struct passwd *usr;
+        if ((usr = getpwuid(getuid())) == NULL) {
+            fprintf(stderr, "\nError: Cannot get passwd entry. %s.\n", strerror(errno));
+            return;
+        }
+
         while (tmp != NULL) {
-            if ((tokens[argc] = (char *) malloc(strlen(tmp) + 1)) == NULL) {
-                fprintf(stderr, "Error: malloc() failed. %s.\n", strerror(errno));
+            if (tmp[0] == '~') {
+                if ((tokens[argc] = (char *) malloc(strlen(tmp) + strlen(usr->pw_dir) + 1)) == NULL) {
+                    fprintf(stderr, "Error: malloc() failed. %s.\n", strerror(errno));
+                    return;
+                }
+                strcpy(tokens[argc], usr->pw_dir);
+                strcpy(tokens[argc] + strlen(tokens[argc]), tmp + 1);
+            } else {
+                if ((tokens[argc] = (char *) malloc(strlen(tmp) + 1)) == NULL) {
+                    fprintf(stderr, "Error: malloc() failed. %s.\n", strerror(errno));
+                    return;
+                }
+                strcpy(tokens[argc], tmp);
             }
-            strcpy(tokens[argc], tmp);
             argc++;
             tmp = strtok(NULL, " ");
         }
